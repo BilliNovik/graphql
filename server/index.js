ed07a5f85230d0c6e4bd8850de61ecd6687392f1
Viacheslav Novik <84062118+BilliNovik@users.noreply.github.com>
2022-06-25 21:02:36 +0300

    add cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { graphqlHTTP } from 'express-graphql'
 import schema from './schema/schema.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = 5000
@@ -11,6 +12,8 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }))
+
+app.use(cors())
 
 mongoose.connect(process.env.MONGOOSE_DB, { dbName: 'graph' })
     .then(() => app.listen(PORT, err => {
